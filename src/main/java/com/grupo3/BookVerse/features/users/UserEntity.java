06cd77c336a21cb9af35ff193a32e0849123d2c4
Yann <81190617+GlavinaLevis@users.noreply.github.com>
2026-04-24 21:34:29 -0300

    feature de UserEntity

---
+++ src/main/java/com/grupo3/BookVerse/features/users/UserEntity.java
@@ -0,0 +1,64 @@
+package com.grupo3.BookVerse.features.users;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private UUID idExternal;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    private Long roleId;
+
+    @Column(nullable = false)
+    private Long suscriptionId;
+
+    /* VER ENUM DESPUES
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status;
+
+    public enum UserStatus {
+        ACTIVE,
+        INACTIVE,
+        SUSPENDED
+    }*/
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+
+}
