bc27abed3e934a6ad4299654bb1ca3091a1447e5
Lucia Ongaro <luuongaro@gmail.com>
2026-05-04 21:15:55 -0300

    feature-ChapterEntity

---
+++ src/main/java/com/grupo3/BookVerse/features/chapters/chapterEntity.java
@@ -0,0 +1,53 @@
+package com.grupo3.BookVerse.features.chapters;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "chapters")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChapterEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "story_id", nullable = false)
+    private Long storyId;
+
+    @Column(name = "chapter_number", nullable = false)
+    private int chapterNumber;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "is_published", nullable = false)
+    private boolean isPublished;
+
+    @Column(name = "is_hidden", nullable = false)
+    private boolean isHidden;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
