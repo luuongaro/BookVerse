34f19a194347915f20e82d9d7967729c1a9fcd9d
Julieta Requena <julirequena15@gmail.com>
2026-05-18 19:59:50 -0300

    ReadingStatus y enumerador creados, group member corregido

---
+++ src/main/java/com/grupo3/BookVerse/features/status/ReadingStatus.java
@@ -0,0 +1,51 @@
+package com.grupo3.BookVerse.features.status;
+import com.grupo3.BookVerse.features.books.BookEntity;
+import com.grupo3.BookVerse.features.story.entity.StoryEntity;
+import com.grupo3.BookVerse.features.users.domain.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "group_members")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class ReadingStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // PK
+
+    @Column(name = "id_external", unique = true, nullable = false)
+    private UUID idExternal = UUID.randomUUID();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user; //FK
+
+    @Column(name = "book_id", nullable = false)
+    private BookEntity bookId; //FK
+
+    @Column(name = "story_id", nullable = false)
+    private StoryEntity storyId; //FK
+
+    @Enumerated(EnumType.STRING) // Guarda el enum como Texto en la base de datos
+    @Column(name = "status", nullable = false)
+    private ReadingStatusEnum status;
+
+    @Column(name = "started_at", nullable = false)
+    private LocalDateTime startedAt;
+
+    @Column(name = "finished_at", nullable = false)
+    private LocalDateTime finishedAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
