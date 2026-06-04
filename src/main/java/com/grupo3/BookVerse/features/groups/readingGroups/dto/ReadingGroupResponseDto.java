61de7ae91c41c389a8cdd49d2d7d49d900869760
Adela Santillan Garcia <adela.avsg@gmail.com>
2026-06-03 23:23:11 -0300

    feat: add ReadingGroup module with DTOs, mapper, and repository methods

---
+++ src/main/java/com/grupo3/BookVerse/features/groups/readingGroups/dto/ReadingGroupResponseDto.java
@@ -0,0 +1,19 @@
+package com.grupo3.BookVerse.features.groups.readingGroups.dto;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record ReadingGroupResponseDto(
+
+        UUID idExternal,
+
+        UUID bookId,
+        UUID createdByUserId,
+
+        String name,
+        Boolean isActive,
+
+        LocalDateTime createdAt
+
+) {
+}
