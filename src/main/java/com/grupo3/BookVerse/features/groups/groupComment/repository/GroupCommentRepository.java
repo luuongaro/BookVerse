0bf63c596f00ca577cac4b0290e33e33f63ffaf7
Adela Santillan Garcia <adela.avsg@gmail.com>
2026-06-05 00:58:39 -0300

    feat: add GroupCommentRepository and refactor GroupCommentMapper package

---
+++ src/main/java/com/grupo3/BookVerse/features/groups/groupComment/repository/GroupCommentRepository.java
@@ -0,0 +1,20 @@
+package com.grupo3.BookVerse.features.groups.groupComment.repository;
+
+
+import com.grupo3.BookVerse.features.groups.groupComment.domain.GroupCommentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface GroupCommentRepository extends JpaRepository<GroupCommentEntity, Long> {
+
+    Optional<GroupCommentEntity> findByIdExternal(UUID idExternal);
+
+    List<GroupCommentEntity> findByGroupIdExternalAndIsHiddenFalseOrderByCreatedAtAsc(UUID groupId);
+
+    List<GroupCommentEntity> findByUserIdExternalAndIsHiddenFalseOrderByCreatedAtDesc(UUID userId);
+}
