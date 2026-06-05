27d6c20cecee6f0150706186e05db38617015c5c
Yann <81190617+GlavinaLevis@users.noreply.github.com>
2026-06-05 01:56:59 -0300

    controller for external API

--- src/main/java/com/grupo3/BookVerse/features/googleBooks/controller/GoogleBooksController.java
+++ src/main/java/com/grupo3/BookVerse/features/googleBooks/controller/GoogleBooksController.java
@@ -1,4 +1,25 @@
 package com.grupo3.BookVerse.features.googleBooks.controller;
 
+import com.grupo3.BookVerse.features.googleBooks.dto.GoogleBooksApiResponseDto;
+import com.grupo3.BookVerse.features.googleBooks.service.GoogleBooksService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/google-books")
+@RequiredArgsConstructor
 public class GoogleBooksController {
+
+    private final GoogleBooksService googleBooksService;
+
+    @GetMapping("/search")
+    public ResponseEntity<GoogleBooksApiResponseDto> searchBooks(@RequestParam String query) {
+        return ResponseEntity.ok(googleBooksService.searchBooks(query));
+    }
+
+
 }
