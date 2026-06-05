736a5d8f576be3af4a7082f529d8ec1d3aca09d4
Yann <81190617+GlavinaLevis@users.noreply.github.com>
2026-06-05 00:18:53 -0300

    Add Google Books API configuration Add Restemplate setup

---
+++ src/main/java/com/grupo3/BookVerse/config/GoogleBooksProperties.java
@@ -0,0 +1,19 @@
+package com.grupo3.BookVerse.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "google.books.api")
+@Getter
+@Setter
+public class GoogleBooksProperties {
+
+    private String url;
+    private String key;
+
+    // Loads Google Books API configuration values from application.yml.
+
+}
