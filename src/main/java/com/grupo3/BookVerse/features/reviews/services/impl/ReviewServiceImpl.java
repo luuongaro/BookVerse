bff70189c440360b24fb6d34ff060f317e945d03
Adela Santillan Garcia <adela.avsg@gmail.com>
2026-06-06 18:34:04 -0300

    refactor: Update ReviewServiceImpl

--- src/main/java/com/grupo3/BookVerse/features/reviews/services/impl/ReviewServiceImpl.java
+++ src/main/java/com/grupo3/BookVerse/features/reviews/services/impl/ReviewServiceImpl.java
@@ -1,67 +1,78 @@
 package com.grupo3.BookVerse.features.reviews.services.impl;
 
-
 import com.grupo3.BookVerse.common.exception.ResourceNotFoundException;
 import com.grupo3.BookVerse.features.reviews.domain.ReviewEntity;
 import com.grupo3.BookVerse.features.reviews.dto.ReviewRequestDto;
 import com.grupo3.BookVerse.features.reviews.dto.ReviewResponseDto;
 import com.grupo3.BookVerse.features.reviews.mapper.ReviewMapper;
 import com.grupo3.BookVerse.features.reviews.repository.ReviewRepository;
-
 import com.grupo3.BookVerse.features.reviews.services.ReviewService;
-import lombok.AllArgsConstructor;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ReviewServiceImpl implements ReviewService {
 
     private final ReviewRepository reviewRepository;
     private final ReviewMapper reviewMapper;
 
     @Override
+    @Transactional(readOnly = true)
     public List<ReviewResponseDto> getAllReviews() {
 
-        return reviewRepository.findAll()
-                .stream()
+        List<ReviewEntity> reviews =
+                reviewRepository.findAll();
+
+        return reviews.stream()
                 .map(reviewMapper::toResponseDto)
                 .toList();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public ReviewResponseDto getReviewById(UUID reviewId) {
 
-        return reviewRepository.findByIdExternal(reviewId)
-                .map(reviewMapper::toResponseDto)
-                        .orElseThrow(() ->
-                                new ResourceNotFoundException(
-                                        "Review not found with id: " + reviewId
-                                ));
+        ReviewEntity review =
+                findReviewByIdExternal(reviewId);
+
+        return reviewMapper.toResponseDto(review);
     }
 
     @Override
+    @Transactional
     public ReviewResponseDto save(ReviewRequestDto reviewRequestDto) {
 
-        ReviewEntity toBeSaved = reviewMapper.toEntity(reviewRequestDto);
+        ReviewEntity review =
+                reviewMapper.toEntity(reviewRequestDto);
 
-        ReviewEntity saved = reviewRepository.save(toBeSaved);
+        ReviewEntity saved =
+                reviewRepository.save(review);
 
         return reviewMapper.toResponseDto(saved);
     }
 
     @Override
+    @Transactional
     public void delete(UUID reviewId) {
 
-        ReviewEntity toBeDeleted = reviewRepository.findByIdExternal(reviewId)
-                .orElseThrow(() ->
-                                new ResourceNotFoundException(
-                                        "Review not found with id: " + reviewId
-                                ));
+        ReviewEntity review =
+                findReviewByIdExternal(reviewId);
 
-        reviewRepository.delete(toBeDeleted);
+        reviewRepository.delete(review);
+    }
+
+    private ReviewEntity findReviewByIdExternal(UUID reviewId) {
+
+        return reviewRepository.findByIdExternal(reviewId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Review not found with id: " + reviewId
+                        )
+                );
     }
 }
