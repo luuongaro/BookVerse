31c6da91568cebb8a19a7a04f4fc4a318b00c3db
Yann <81190617+GlavinaLevis@users.noreply.github.com>
2026-06-06 18:01:29 -0300

    improve for reading group feature

--- src/main/java/com/grupo3/BookVerse/features/groups/readingGroups/mappers/ReadingGroupMapper.java
+++ src/main/java/com/grupo3/BookVerse/features/groups/readingGroups/mappers/ReadingGroupMapper.java
@@ -3,14 +3,11 @@ package com.grupo3.BookVerse.features.groups.readingGroups.mappers;
 import com.grupo3.BookVerse.features.groups.readingGroups.domain.ReadingGroupEntity;
 import com.grupo3.BookVerse.features.groups.readingGroups.dto.ReadingGroupRequestDto;
 import com.grupo3.BookVerse.features.groups.readingGroups.dto.ReadingGroupResponseDto;
-import com.grupo3.BookVerse.features.books.domain.BookEntity;
-import com.grupo3.BookVerse.features.users.domain.UserEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
-import java.util.UUID;
 
 @Mapper(
         componentModel = "spring",
@@ -20,8 +17,8 @@ public interface ReadingGroupMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "idExternal", ignore = true)
-    @Mapping(target = "book", source = "bookId")
-    @Mapping(target = "createdBy", source = "createdByUserId")
+    @Mapping(target = "book", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
     @Mapping(target = "comments", ignore = true)
     @Mapping(target = "members", ignore = true)
     @Mapping(target = "progresses", ignore = true)
@@ -34,21 +31,4 @@ public interface ReadingGroupMapper {
     ReadingGroupResponseDto toResponseDto(ReadingGroupEntity entity);
 
     List<ReadingGroupResponseDto> toResponseDtoList(List<ReadingGroupEntity> entities);
-    default BookEntity mapBook(UUID bookId) {
-        if (bookId == null) {
-            return null;
-        }
-        BookEntity book = new BookEntity();
-        book.setIdExternal(bookId);
-        return book;
-    }
-
-    default UserEntity mapUser(UUID userId) {
-        if (userId == null) {
-            return null;
-        }
-        UserEntity user = new UserEntity();
-        user.setIdExternal(userId);
-        return user;
-    }
 }
