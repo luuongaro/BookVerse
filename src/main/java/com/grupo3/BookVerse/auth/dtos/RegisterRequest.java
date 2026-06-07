4a1cf2a29f667d50c37cb5c78d341a666204be3f
Adela Santillan Garcia <adela.avsg@gmail.com>
2026-06-07 16:19:09 -0300

    Add DTOs for authentication and registration requests

---
+++ src/main/java/com/grupo3/BookVerse/auth/dtos/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.grupo3.BookVerse.auth.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record RegisterRequest(
+
+        @NotBlank(message = "Username is required")
+        String username,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        String password
+
+) {
+}
