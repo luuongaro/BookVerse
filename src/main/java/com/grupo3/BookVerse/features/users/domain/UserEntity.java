a2e5c9983c3a46c5a0044b5a3b8106f86031b16e
Yann <81190617+GlavinaLevis@users.noreply.github.com>
2026-06-07 19:44:48 -0300

    fixes on userEnt

--- src/main/java/com/grupo3/BookVerse/features/users/domain/UserEntity.java
+++ src/main/java/com/grupo3/BookVerse/features/users/domain/UserEntity.java
@@ -56,16 +56,6 @@ public class UserEntity implements UserDetails {
     @Column(name = "status", nullable = false)
     private UserStatus status;
 
-    @PrePersist
-    protected void onCreate() {
-        if (idExternal == null) {
-            idExternal = UUID.randomUUID();
-        }
-        if (status == null) {
-            status = UserStatus.ACTIVE;
-        }
-    }
-
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
@@ -110,6 +100,16 @@ public class UserEntity implements UserDetails {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
     private List<ReadingStatusEntity> readingStatuses = new ArrayList<>();
+
+    @PrePersist
+    protected void onCreate() {
+        if (idExternal == null) {
+            idExternal = UUID.randomUUID();
+        }
+        if (status == null) {
+            status = UserStatus.ACTIVE;
+        }
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
