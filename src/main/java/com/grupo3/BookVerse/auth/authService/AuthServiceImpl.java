693431ebe299ce79edff13f70d3e9d6fa2160c50
Adela Santillan Garcia <adela.avsg@gmail.com>
2026-06-07 16:50:19 -0300

    Add AuthController and implement authentication and registration endpoints

--- src/main/java/com/grupo3/BookVerse/auth/authService/AuthServiceImpl.java
+++ src/main/java/com/grupo3/BookVerse/auth/authService/AuthServiceImpl.java
@@ -1,25 +1,36 @@
 package com.grupo3.BookVerse.auth.authService;
-
 import com.grupo3.BookVerse.auth.dtos.AuthRequest;
 import com.grupo3.BookVerse.auth.dtos.AuthResponse;
+import com.grupo3.BookVerse.auth.dtos.RegisterRequest;
 import com.grupo3.BookVerse.auth.jwt.JwtService;
+import com.grupo3.BookVerse.common.exception.ResourceNotFoundException;
+import com.grupo3.BookVerse.features.subscriptions.domain.SubscriptionEntity;
+import com.grupo3.BookVerse.features.subscriptions.domain.SubscriptionType;
+import com.grupo3.BookVerse.features.subscriptions.repository.SubscriptionRepository;
 import com.grupo3.BookVerse.features.users.domain.UserEntity;
+import com.grupo3.BookVerse.features.users.dto.UserRequestDto;
+import com.grupo3.BookVerse.features.users.dto.UserResponseDto;
 import com.grupo3.BookVerse.features.users.repository.UserRepository;
+import com.grupo3.BookVerse.features.users.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
-    private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final UserService userService;
+    private final SubscriptionRepository subscriptionRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public AuthResponse authenticate(AuthRequest request) {
 
         authenticationManager.authenticate(
@@ -39,8 +50,30 @@ public class AuthServiceImpl implements AuthService {
                         )
                 );
 
-        String jwtToken = jwtService.generateToken(user);
+        String token = jwtService.generateToken(user);
 
-        return new AuthResponse(jwtToken);
+        return new AuthResponse(token);
+    }
+
+    @Override
+    @Transactional
+    public UserResponseDto register(RegisterRequest request) {
+
+        SubscriptionEntity freeSubscription =
+                subscriptionRepository.findByType(SubscriptionType.FREE)
+                        .orElseThrow(() ->
+                                new ResourceNotFoundException(
+                                        "Default FREE subscription not found"
+                                )
+                        );
+
+        UserRequestDto userRequestDto = new UserRequestDto(
+                request.username(),
+                request.email(),
+                request.password(),
+                freeSubscription.getIdExternal()
+        );
+
+        return userService.createUser(userRequestDto);
     }
 }
