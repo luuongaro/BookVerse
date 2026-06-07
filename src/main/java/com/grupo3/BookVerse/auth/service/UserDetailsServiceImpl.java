777823922e5bd83decb03138e7cb3e078dd2a127
Yann <81190617+GlavinaLevis@users.noreply.github.com>
2026-06-07 15:04:16 -0300

    UserDetailsServiceImpl and SecurityConfig added

---
+++ src/main/java/com/grupo3/BookVerse/auth/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.grupo3.BookVerse.auth.service;
+
+import com.grupo3.BookVerse.features.users.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmailWithRoles(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email: " + email)
+                );
+    }
+}
